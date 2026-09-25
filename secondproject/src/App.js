c1220b0d863ed9251e3a1bdc058cb50ad99b8a1b
atrubaabid <atrubaabid7@gmail.com>
2026-09-25 07:31:39 -0700

    Done

--- secondproject/src/App.js
+++ secondproject/src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className='bg-red-900 text-white mt-[100px] p-[100px] text-2xl'>Syeda Atruba</h1>
+
+      {/* 13. How to Setup Tailwind CSS in React */}
+      
+      <h1 className='bg-red-900 text-white mt-[100px] p-[100px] text-2xl'>Syeda Atruba</h1>
+
+
     </div>
   );
 }
